d5a374dfc6a39ea65efb95e8438b6a528c954537
Christian Ray Leovido <christian.leovido@gmail.com>
2020-04-04 20:24:27 +0100

    Initial Commit

---
+++ SwiftyRamda/SwiftyRamda.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyRamda.h
+//  SwiftyRamda
+//
+//  Created by Christian Leovido on 04/04/2020.
+//  Copyright © 2020 Christian Leovido. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyRamda.
+FOUNDATION_EXPORT double SwiftyRamdaVersionNumber;
+
+//! Project version string for SwiftyRamda.
+FOUNDATION_EXPORT const unsigned char SwiftyRamdaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyRamda/PublicHeader.h>
+
+
